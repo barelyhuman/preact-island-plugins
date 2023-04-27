497c805b425e6eda187f71c04beebabfc7c13fe1
reaper <ahoy@barelyhuman.dev>
2023-04-27 06:42:06 +0530

    fix: nested islands
    restore tree with nested islands by
    going through the initial rendered nodes
    and using their names to recreate the
    nodes instead of creating invalid nodes

--- src/lib/island.ts
+++ src/lib/island.ts
@@ -98,19 +98,56 @@ const restoreTree = (type, props={}) => {
     return h(
       type,
       props,
-      restoreTree(props.children.type || '', props.children.props || {})
+      restoreTree(props.children.type, props.children.props || {})
     )
   }
 
   return h(type, props)
 }
 
+
+function mergePropsWithDOM(rootNode, props) {
+  const scripts = new Map()
+  function walk(node, propPoint) {
+    Array.from((node.childNodes||[]))
+    .filter(x=>{
+      if(x.localName!="script"){
+        return true
+      }else{
+        scripts.set(x.src,x)
+      }
+    })
+    .forEach((child,index)=>{
+      let _prop = propPoint
+      if(Array.isArray(propPoint) && propPoint[index]){
+        _prop = propPoint[index]
+      } 
+      if(/^island-(.*)+/.test(child.localName)){
+        _prop.type = child.localName
+        if(_prop.props){
+          _prop.props["data-props"]=JSON.stringify(_prop.props)
+        }
+      }
+      if(child.childNodes.length>0){
+        if(propPoint.children){
+          walk(child,propPoint.children)
+        }
+      }
+    })
+  }
+  walk(rootNode, props)
+  rootNode.innerHTML = ""
+  const scriptNodes = [...scripts].map(([k,v])=>v)
+  rootNode.append(...scriptNodes)
+}
+
 customElements.define("${islandName}", class Island${name} extends HTMLElement { 
   async connectedCallback() {
       const c = await import(${JSON.stringify(importPath)}); 
       const props = JSON.parse(this.dataset.props  || '{}'); 
-      hydrate(restoreTree(c.default, props, props.children || []), this)
-  } 
+      mergePropsWithDOM(this,props);
+      hydrate(restoreTree(c.default, props), this)
+  }
 })`
 }
 
@@ -139,14 +176,6 @@ function buildIslandServer(
       }
     }
     if (child.type === 'ImportDeclaration') {
-      if (
-        child.source.type === 'StringLiteral' &&
-        child.source.value.match(/.island\.?(jsx?|tsx?)?$/)
-      ) {
-        throw new Error(
-          `Importing Island inside another island, isn't supported yet, imported ${child.source.value} in ${sourcePath}`
-        )
-      }
       if (
         child.source.type === 'StringLiteral' &&
         child.source.value === 'preact'
