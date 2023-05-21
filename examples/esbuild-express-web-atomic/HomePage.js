50e4b6bf7a7dc3eeb1b370c8bdd1e787f7895913
reaper <ahoy@barelyhuman.dev>
2023-05-21 16:48:24 +0530

    add base impl and example

--- examples/esbuild-express-web-atomic/HomePage.js
+++ examples/esbuild-express-web-atomic/HomePage.js
@@ -1,13 +1,21 @@
 import Counter from './Counter.island.js'
+import Todo from './Todo.js'
 
-export default function () {
+export default function() {
   const initData = { initValue: 10 }
   return (
     <>
       <section>
         <h1>Hello from Preact Islands</h1>
       </section>
-      <Counter {...initData} />
+      <div>
+        <h1>Counter</h1>
+        <Counter {...initData} />
+      </div>
+      <div>
+        <h1>Todo</h1>
+        <Todo />
+      </div>
     </>
   )
 }
